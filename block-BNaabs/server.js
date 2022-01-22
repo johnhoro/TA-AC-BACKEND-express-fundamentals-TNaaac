65331c416577ef6421304705443e35fa3d2850f4
john horo <johnhoro125@gmail.com>
2022-01-22 13:29:20 +0530

    refactor

--- block-BNaabs/server.js
+++ block-BNaabs/server.js
@@ -1,21 +1,33 @@
 var express = require(`express`);
+var cookieParser = require(`cookie-parser`);
 var logger = require(`morgan`);
 
 var app = express();
 
-app.use(express.jason());
-app.use(express.urlencoded({ extend: false }));
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/public"));
+app.use(logger(`dev`));
 
+//routes
 app.get(`/`, (req, res) => {
-  res.sendFile(__dirname + `/index.html`);
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.get(`/new`, (req, res) => {
-  res.sendFile(__dirname + `/new.html`);
+  res.sendFile(__dirname + "/new.html");
 });
 
-app.use(`post`, (req, res) => {});
+app.post(`/new`, (req, res) => {
+  res.json(req.body);
+});
 
-app.listen(2222, () => {
-  console.log(`server listening on port 2222`);
+app.get(`/users/:username`, (req, res) => {
+  var username = req.params.username;
+  res.send(username);
+});
+
+app.listen(4000, () => {
+  console.log(`server listening on port 4k`);
 });
